c0ceee9326b1003cc872c32d9a0c35a44e156a3f
ney2thehey <shetty.saineha@gmail.com>
2023-03-09 13:48:55 -0600

    updated src file structure with components

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+function Navbar() {
+    return (
+      <nav className="orange darken-2" role="navigation">
+        <div className="nav-wrapper container">
+          <a id="logo-container" href="#" className="brand-logo">
+            Big 12 Scoragami
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <a href="faq.html">How to use</a>
+            </li>
+            <li>
+              <a href="faq.html">FAQ</a>
+            </li>
+            <ul>
+              <li>
+                <a href="#contact">Contact us</a>
+              </li>
+            </ul>
+          </ul>
+  
+          <ul id="nav-mobile" className="sidenav">
+            <li>
+              <a href="#">Navbar Link</a>
+            </li>
+            <a href="faq.html">
+              <div className="darken-2">
+                <i className="material-icons">question_answer</i>
+                <span className="flow-text">FAQ</span>
+              </div>
+            </a>
+  
+            <div className=" orange darken-2">
+              <i className="material-icons">question_answer</i>
+              <span className="flow-text">Color blind</span>
+            </div>
+  
+            <div className="orange darken-2">
+              <i className="material-icons">question_answer</i>
+              <span className="flow-text">How does it work</span>
+            </div>
+          </ul>
+  
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+  
