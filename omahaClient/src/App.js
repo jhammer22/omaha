07678ac3dad01d726290fdc7e58f61bad5c05fe0
SeanAllen19 <saallen19.dev@gmail.com>
2023-03-11 16:20:56 -0600

    Made Navbar links to work

--- omahaClient/src/App.js
+++ omahaClient/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Section from './components/Section';
 import ScoreTable from './components/ScoreTable';
+import Faq from './pages/Faq';
+import Login from './pages/Login'
 
 
 function App() {
 
   return (
-    <> 
+    <>
+   <Router>
+
+ 
     <Navbar />
+    
+    <Switch>
+      <Route path="/faq" component={Faq} />
+      <Route path="/login" component={Login} />
+    </Switch>
+   </Router>
+    
     <Section />
     <ScoreTable />
     </>
