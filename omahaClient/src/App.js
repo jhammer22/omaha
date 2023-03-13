c320b3e275878bae8eec08c4a919479175ae08dd
SeanAllen19 <saallen19.dev@gmail.com>
2023-03-13 12:06:53 -0500

    Consolidated into Home page and minior cleanup

--- omahaClient/src/App.js
+++ omahaClient/src/App.js
@@ -1,29 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Comopnents Section
 import Navbar from './components/Navbar';
-import Section from './components/Section';
-import ScoreTable from './components/ScoreTable';
-import Faq from './pages/Faq';
-import Login from './pages/Login'
 
+//// Pages Section
+import Home from './pages/Home';
+import Faq from './pages/Faq';
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <>
    <Router>
-
- 
     <Navbar />
-    
     <Switch>
+      <Route path="/home" component={Home} />
       <Route path="/faq" component={Faq} />
       <Route path="/login" component={Login} />
     </Switch>
    </Router>
-    
-    <Section />
-    <ScoreTable />
     </>
   );
 }
