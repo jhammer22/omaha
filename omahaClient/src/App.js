0ba0a62f30e9047c08bc24b785967abcd1277625
ney2thehey <shetty.saineha@gmail.com>
2023-03-13 14:17:14 -0500

    updated login page and js

--- omahaClient/src/App.js
+++ omahaClient/src/App.js
@@ -5,6 +5,7 @@ import Section from './components/Section';
 import ScoreTable from './components/ScoreTable';
 import Faq from './pages/Faq';
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 
 function App() {
@@ -19,11 +20,12 @@ function App() {
     <Switch>
       <Route path="/faq" component={Faq} />
       <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </Switch>
    </Router>
     
-    <Section />
-    <ScoreTable />
+    {/* <Section />
+    <ScoreTable /> */}
     </>
   );
 }
