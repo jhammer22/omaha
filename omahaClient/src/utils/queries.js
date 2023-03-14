4f299a8f5d2eb93132fcf30759d04ce60a65284d
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-13 22:11:50 -0500

    resolved merge conflicts

--- omahaClient/src/utils/queries.js
+++ omahaClient/src/utils/queries.js
@@ -82,3 +82,27 @@ export const QUERY_ME = gql`
     }
   }
 `;
+export const QUERY_GAMES = gql`
+  query getGames {
+    games {
+      _id
+      season
+      away_team
+      away_points
+      home_team
+      home_points
+      home_id
+      away_id
+    }
+  
+  }
+`;
+
+// export const QUERY_SINGLE_GAME = gql`
+//   query getSingleGame($gameId: ID!) {
+//     game(gameId: $gameId) {
+//       _id
+
+//     }
+//   }
+// `
