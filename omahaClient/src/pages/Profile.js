745df129937314a8fdd387b50e19800ef069ff46
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-15 11:41:09 -0500

    touched the coconut again

--- omahaClient/src/pages/Profile.js
+++ omahaClient/src/pages/Profile.js
@@ -44,9 +44,33 @@ useEffect(() => {scoreTable()});
 //     );
 //   }
 
-  return (
 
-    
+function scoreTable() {
+    const scores = gamesData?.games.filter((game) => {
+        // let homeScores = game.home_points;
+        // let awayScores = game.away_points;
+        // console.log(homeScores);
+        // console.log(awayScores);
+        // for (let i = 0; i < game.length; i++) {
+            let appendVal  
+            console.log(game);
+            if (game.home_points > game.away_points) {
+                appendVal = 'r' + game.away_points + '-c' + game.home_points
+                console.log(appendVal)
+                document.getElementById(appendVal)?.setAttribute("style", "background-color: #BF5700");
+                
+            } else if (game.home_points < game.away_points) {
+                appendVal = 'r' + game.home_points + '-c' + game.away_points
+                document.getElementById(appendVal)?.setAttribute("style", "background-color: #BF5700");
+            }
+        // } //return game;
+    })
+   
+    console.log(scores);
+  };
+  
+
+  return (
     <div className="section no-pad-bot" id="index-banner">
     <div className="container">
         <br /><br />
