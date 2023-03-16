95e3e39665140b0a709b3aa0335c66fab24b46a5
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-16 02:56:51 -0500

    solved merge conflicts

--- omahaClient/src/components/Deuteranopia.js
+++ omahaClient/src/components/Deuteranopia.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import "../styles/NavbarStyles.css";
 
 function ScoreTable(){
