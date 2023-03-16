7b845b033b454e9af0660ae079a9b62b071a118c
RyanDamron <119913449+RyanDamron@users.noreply.github.com>
2023-03-15 20:21:23 -0500

    cleaned up and working

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -9,19 +9,3 @@ db.once('open', async () => {
   process.exit(0);
 });
 
-
-// need to grab home and away scores from db for each game map over scores and sort high and low number then asign values to corresponding table id
-
-// example game object from games.json
-// {
-//   "season": 2022,
-//   "away_team": "Iowa State",
-//   "away_points": 14,
-//   "home_team": "TCU",
-//   "home_points": 62,
-//   "id": 401404120,
-//   "home_id": 2628,
-//   "away_id": 66
-// },
-
-// db.collection.find()  dot notation; e.g. "field.nestedfield": <value>
